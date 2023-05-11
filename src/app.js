8808adec57f5dfc7e7566a4d68872a5aed4bd068
Peter Kiraly <peter.kiral@gmail.com>
2023-05-11 20:01:57 +0200

    token kiolvasása működik

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const doc = {
-   
+   addButton: document.querySelector('#addButton')
 };
 
 const state = {
-    
+    token: null
 };
 
-window.addEventListener('load', ()=> {});
+window.addEventListener('load', ()=> {
+    init()
+});
 
-function init (){};
+function init (){
+    // VAGY: doc.addButton.addEventListener('click', startAdding); // ha egy sort írunk akkor érdemes...
+    doc.addButton.addEventListener('click', startAdding);
+};
 
+// kinyerjük a lokális tárolóból a tokent
+function startAdding(){
+    console.log(localStorage.getItem('token'))
+}
--- src/login.js
+++ src/login.js
@@ -7,7 +7,7 @@ const doc = {
 const state = {
     user: null,
     pass: null,
-    host: 'http://localhost:8000/'
+    host: 'http://localhost:8000/api/'
 };
 
 window.addEventListener('load', ()=> {
@@ -18,16 +18,37 @@ function init (){
     doc.loginButton.addEventListener('click', ()=> {
         startLogin();
     });
-};
+}
 
 function startLogin(){
     state.user = doc.userInput.value;
     state.pass = doc.passInput.value;
     login();
-};
+}
 
 function login(){
     let endpoint = 'login';
     let url = state.host+endpoint;
-    fetch(url);
-};
+
+    // úgy mint Insomniában..
+    let userData = {
+        name: state.user,
+        password: state.pass
+    };
+
+    fetch(url, {
+        method: 'post',
+        // Stringesíteni kell...
+        body: JSON.stringify (userData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(result => {
+        // console.log(result)
+        localStorage.setItem('name', result.name);
+        localStorage.setItem('token', result.token);
+
+    });
+}
